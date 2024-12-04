bead486f4e32d503e2dcba81c2fce070b39fb419
Yann <yann@offroadlabs.com>
2024-12-04 10:31:46 +0100

    end of project

---
+++ src/app/safe-link.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, inject, input } from '@angular/core';
+import { LogDirective } from './log.directive';
+
+@Directive({
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
+  hostDirectives: [LogDirective],
+})
+export class SafeLinkDirective {
+  queryParam = input<string>('myapp', { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+
+    if (wantsToLeave) {
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href =
+        address + '?from=' + this.queryParam();
+      return;
+    }
+
+    event.preventDefault();
+  }
+}
